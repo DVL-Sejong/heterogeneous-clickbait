a26374a6033bfc404ddbe3c41435f203bded726b
skang132 <skang132@users.noreply.github.com>
2024-05-29 09:50:35 +0000

    deploy: 4fa2bea86f425773dbd3a3c34548130e27774340

---
+++ static/js/window.js
@@ -0,0 +1,20 @@
+function scaleDashboard() {
+    const dashboard = document.querySelector('.dashboard');
+    const layoutSidenavContent = document.querySelector('#layoutSidenav_content');
+    
+    const scaleX = layoutSidenavContent.offsetWidth / 1330;
+    const scaleY = layoutSidenavContent.offsetHeight / 1045;
+    const scale = Math.min(scaleX, scaleY);
+    
+    if (layoutSidenavContent.offsetWidth < 1680) {
+        dashboard.style.transform = `scale(${scale})`;
+    }
+    else {
+        dashboard.style.transform = `scale(${scaleX*0.99}, ${scaleY})`;
+    }
+    
+    dashboard.style.transformOrigin = '0 0px';
+}
+
+window.addEventListener('resize', scaleDashboard);
+scaleDashboard();
